1f73313db17c806aeb0e4c297cffd82a2c483b7f
Felix Ståhl <31844847+felixstahl@users.noreply.github.com>
2024-12-04 20:58:05 +0100

    started aoc day 1

--- AOC1.py
+++ AOC1.py
@@ -1,1 +1,18 @@
-Advent of Code
+inputFile = open('AOC1input.txt').read().strip()
+
+listOfLines = inputFile.split('\n')
+
+listOfLefts = []
+listOfRights = []
+
+for line in listOfLines:
+    left, right = line.split()
+    listOfLefts.append(left)
+    listOfRights.append(right)
+
+listOfLefts = sorted(left)
+listOfRights = sorted(right)
+
+
+#no more time today. got to go
+
